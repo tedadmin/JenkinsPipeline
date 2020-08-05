8c559cc2a72135239f0d54caad12ad113127c80d
Jithin-Muringoly <60697964+Jithin-Muringoly@users.noreply.github.com>
2020-08-06 02:23:05 +0530

    Update container.sh

--- container.sh
+++ container.sh
@@ -8,7 +8,7 @@ ls
 docker ps -a
 echo "my container id is ${conatinerId}"
 docker start ${conatinerId}
-docker exec -i ${conatinerId} bin/bash
+docker exec -it ${conatinerId} bin/bash
 ls
 aws --version
 node --version
