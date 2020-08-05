872136c8c9ac5a8a257858fdb28b358ce09b7795
Jithin-Muringoly <60697964+Jithin-Muringoly@users.noreply.github.com>
2020-08-05 22:07:47 +0530

    Update container.sh

--- container.sh
+++ container.sh
@@ -1,6 +1,6 @@
 docker ps
 docker build . --tag customdocker:1.0
-conatinerId=$(docker ps -aqf "name=customdocker")
+conatinerId=$(docker ps -aqf "name=customdocker:1.0")
 ls -ltr               
 docker run  customdocker:1.0
 docker ps
