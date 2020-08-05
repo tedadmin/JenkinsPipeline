400d6946a9c97ae96450d0d0f12048fbe39444d3
Jithin-Muringoly <60697964+Jithin-Muringoly@users.noreply.github.com>
2020-08-06 01:40:07 +0530

    Update container.sh

--- container.sh
+++ container.sh
@@ -1,8 +1,8 @@
 docker ps
-docker build . --tag customdocker:3.0
+//docker build . --tag customdocker:3.0
 ls -ltr               
-docker run --name one customdocker:3.0
-docker run -d --name  two customdocker:2.0  --restart always
+docker run -d --name four jmuringoly/ted:0.5 --restart always
+//docker run -d --name  two customdocker:2.0  --restart always
 conatinerId=$(docker ps -a | awk 'NR > 1 {print $1; exit}')
 docker ps
 ls
