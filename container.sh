41da6a9106579913af7b2b8e0d81c64480852799
Jithin-Muringoly <60697964+Jithin-Muringoly@users.noreply.github.com>
2020-08-05 21:23:54 +0530

    Create container.sh

---
+++ container.sh
@@ -0,0 +1,10 @@
+docker ps
+docker build . -t customdocker
+conatinerId=$(docker ps -aqf "name=customdocker") > containerid.txt
+ls -ltr               
+docker run  "customdocker"
+docker ps
+ls
+docker exec -it $conatinerId bash
+aws --version
+node --version
