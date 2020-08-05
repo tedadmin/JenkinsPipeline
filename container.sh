57ce20edc486db790eaa214ad93981593f0ba726
Jithin-Muringoly <60697964+Jithin-Muringoly@users.noreply.github.com>
2020-08-05 21:59:39 +0530

    Update container.sh

--- container.sh
+++ container.sh
@@ -1,11 +1,11 @@
 docker ps
-docker build . -t customdocker
+docker build . --tag customdocker:1.0
 conatinerId=$(docker ps -aqf "name=customdocker")
 ls -ltr               
 docker run  "customdocker"
 docker ps
 ls
 echo "my container id is ${conatinerId}"
-docker exec -it customdocker:latest bash
+docker exec -it customdocker:1.0 bash
 aws --version
 node --version
