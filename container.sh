f60bb2b51df0e066938d5379e6df5645df268a1e
Jithin-Muringoly <60697964+Jithin-Muringoly@users.noreply.github.com>
2020-08-05 23:47:10 +0530

    Update container.sh

--- container.sh
+++ container.sh
@@ -1,7 +1,7 @@
 docker ps
 docker build . --tag customdocker:2.0
 ls -ltr               
-docker run -d customdocker:2.0
+docker run customdocker:2.0
 conatinerId=$(docker ps -a | awk 'NR > 1 {print $1; exit}')
 docker ps
 ls
