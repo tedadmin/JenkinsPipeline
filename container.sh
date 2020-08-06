67398e50885cdcb51ef3d5df449c618c2cc28e5e
Jithin-Muringoly <60697964+Jithin-Muringoly@users.noreply.github.com>
2020-08-06 11:10:33 +0530

    Update container.sh

--- container.sh
+++ container.sh
@@ -1,14 +1,14 @@
 docker ps
-docker build . --tag customdocker:4.0
+#docker build . --tag customdocker:4.0
 ls -ltr               
-docker run -d  customdocker:4.0 
+docker run -d  --name 060800 jmuringoly/ted:0.5 
 conatinerId=$(docker ps -a | awk 'NR > 1 {print $1; exit}')
 docker ps
 ls
 docker ps -a
 echo "my container id is ${conatinerId}"
-docker start ${conatinerId}
-docker exec -i ${conatinerId} bin/bash
+#docker start ${conatinerId}
+docker exec -i ${conatinerId} bash
 ls
 aws --version
 node --version
