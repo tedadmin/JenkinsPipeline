4e3d09847200fbb4ab73aed2477f6eb7c1b5a69b
Jithin-Muringoly <60697964+Jithin-Muringoly@users.noreply.github.com>
2020-08-06 11:10:59 +0530

    Update container.sh

--- container.sh
+++ container.sh
@@ -1,7 +1,7 @@
 docker ps
 #docker build . --tag customdocker:4.0
 ls -ltr               
-docker run -d  --name 060800 jmuringoly/ted:0.5 
+docker run -d  --name 060800 jmuringoly/ted:0.5 -no-cache
 conatinerId=$(docker ps -a | awk 'NR > 1 {print $1; exit}')
 docker ps
 ls
