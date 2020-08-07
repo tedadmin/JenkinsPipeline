be42cc8eb57fd7c94bf209a20ed3c7a312b7c65a
tedadmin <67967678+tedadmin@users.noreply.github.com>
2020-08-07 12:55:29 +0530

    Update container.sh

--- container.sh
+++ container.sh
@@ -1,12 +1,16 @@
 docker ps
+echo " buiding nthe image"
 docker build . --tag customdocker:4.0
+echo "image bhuils"
 ls -ltr               
 docker run --name 060800 customdocker:4.0 -no-cache
-conatinerId=$(docker ps -a | awk 'NR > 1 {print $1; exit}')
+conatinerId1=$(docker ps -a | awk 'NR > 1 {print $1; exit}')
+containerId2=$(docker ps --filter ancestor=ted-docker:latest --format "{{.ID}}")
 docker ps
 ls
 docker ps -a
-echo "my container id is ${conatinerId}"
+echo "my container id is ${conatinerId1}"
+echo "my container id is ${conatinerId2}"
 #docker start ${conatinerId}
 #docker exec -i ${conatinerId} bash
 #ls
